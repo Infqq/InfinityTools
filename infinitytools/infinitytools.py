62112a9114362d4090d1360914b2cede418a4e83
Infqq <57950063+Infqq@users.noreply.github.com>
2020-11-19 10:27:37 +0400

    Проверка открытых портов.
    Новая утилита - scan()

--- infinitytools/infinitytools.py
+++ infinitytools/infinitytools.py
@@ -1,5 +1,9 @@
 from pycbrf import ExchangeRates, Banks
 from time import gmtime, strftime
+import socket
+
+mas = [20, 21, 22, 23, 25, 42, 43, 53, 67, 69, 80, 110, 115, 123, 137, 138, 139, 143, 161, 179, 443, 445, 514, 515, 993, 995]
+
 
 class currency():
         def get(self, name: str):
@@ -7,3 +11,18 @@ class currency():
                 rates = ExchangeRates(tt, locale_en=True)
                 result = rates[name].rate
                 return result
+
+class scan():
+        def get(self, host: str):
+                p = f'Открытые порты {host}:\n'
+                for port in mas:
+                        s = socket.socket()
+                        s.settimeout(1)
+                        try:
+                                s.connect((host, port))
+                        except socket.error:
+                                pass
+                        else:
+                                s.close
+                                p = p + str(port) + ' '
+                                return p
